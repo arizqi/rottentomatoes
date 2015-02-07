b9d3b33d3af3dbd1523798a7753fc8513c67371e
Ashar Rizqi <ashar@apples-MBP.hsd1.ca.comcast.net>
2015-02-07 01:17:51 -0800

    Initial Commit

---
+++ rotten_tomatoes_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rotten_tomatoes_1
+//
+//  Created by Ashar Rizqi on 2/7/15.
+//  Copyright (c) 2015 Ashar Rizqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
